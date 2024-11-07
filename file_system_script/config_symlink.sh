a2ec0c56fadfa231ff10fef724b934b3bb0b1d6c
Henry Wong <hwong226@my.bcit.ca>
2024-11-06 22:18:10 -0800

    changed then placement for my own preference.

--- file_system_script/config_symlink.sh
+++ file_system_script/config_symlink.sh
@@ -19,7 +19,8 @@ clone_dir=$USER_HOME/acit2420_shell_script/file_system_script/2420-as2-starting-
 
 # symlink bin
 # Check if $USER_HOME/bin doesn't exist. If it doesn't created the bin dir, else, say it already exist.
-if [ ! -d $USER_HOME/bin ]; then
+if [ ! -d $USER_HOME/bin ];
+then
   mkdir -p $USER_HOME/bin
   echo "/bin directory created"
 else
@@ -27,14 +28,16 @@ else
 fi
 
 # Check if the symlink exist with -L option. If it exist, say it exist, else symlink the two and say it has been created.
-if [ -L $USER_HOME/bin/sayhi ]; then
+if [ -L $USER_HOME/bin/sayhi ];
+then
   echo "Symlink $USER_HOME/bin/sayhi already exists"
 else
   ln -s $clone_dir/bin/sayhi $USER_HOME/bin/sayhi
   echo "Symlink $USER_HOME/bin/sayhi created"
 fi
 
-if [ -L $USER_HOME/bin/install-fonts ]; then
+if [ -L $USER_HOME/bin/install-fonts ];
+then
   echo "Symlink $USER_HOME/bin/install-fonts already exists"
 else
   ln -s $clone_dir/bin/install-fonts $USER_HOME/bin/install-fonts
@@ -43,7 +46,8 @@ fi
 
 # symlink config for kak
 # Check if $USER_HOME/.config/kak doesn't exist. If it doesn't created the .config/kak dir, else, say it already exist.
-if [ ! -d $USER_HOME/.config/kak ]; then
+if [ ! -d $USER_HOME/.config/kak ];
+then
   mkdir -p $USER_HOME/.config/kak
   echo "/.config/kak directory created"
 else
@@ -51,7 +55,8 @@ else
 fi
 
 # If symlink exist, say it exist, else symlink the two and say it has been created.
-if [ -L $USER_HOME/.config/kak/kakrc ]; then
+if [ -L $USER_HOME/.config/kak/kakrc ];
+then
   echo "Symlink $USER_HOME/.config/kak/kakrc already exists"
 else
   ln -s $clone_dir/config/kak/kakrc $USER_HOME/.config/kak/kakrc
@@ -60,7 +65,8 @@ fi
 
 # symlink config for tmux
 # Check if $USER_HOME/.config/tmux doesn't exist. If it doesn't created the .config/tmux dir, else, say it already exist.
-if [ ! -d $USER_HOME/.config/tmux ]; then
+if [ ! -d $USER_HOME/.config/tmux ];
+then
   mkdir -p $USER_HOME/.config/tmux
   echo "/.config/tmux directory created"
 else
@@ -68,7 +74,8 @@ else
 fi
 
 # If symlink exist, say it exist, else symlink the two and say it has been created.
-if [ -L $USER_HOME/.config/tmux/tmux.conf ]; then
+if [ -L $USER_HOME/.config/tmux/tmux.conf ];
+then
   echo "Symlink $USER_HOME/.config/tmux/tmux.conf already exists"
 else
   ln -s $clone_dir/config/tmux/tmux.conf $USER_HOME/.config/tmux/tmux.conf
@@ -77,7 +84,8 @@ fi
 
 # symlink home bashrc
 # If symlink exist, say it exist, else symlink the two and say it has been created.
-if [ -L $USER_HOME/.bashrc ]; then
+if [ -L $USER_HOME/.bashrc ];
+then
   echo "Symlink $USER_HOME/.bashrc already exists"
 else
   ln -s $clone_dir/home/bashrc $USER_HOME/.bashrc
