3ae44b66b3ddf5a9af3e5cb0f05c352f8299b0db
Henry Wong <hwong226@my.bcit.ca>
2024-11-07 15:06:30 -0800

    added more comments to explain what the script does. Also added proper documentation about the script and the sources used in the script.

--- file_system_script/config_symlink.sh
+++ file_system_script/config_symlink.sh
@@ -1,47 +1,59 @@
 #!/usr/bin/env bash
 
+# ==========================================================================================================================
+# Script Name: config_symlink.sh
+# Description: This is the symlink config script that will symlink with the repo config that's in the directory.
+# Author: Henry Wong
+# Date: 2024-11-05
+# Version: 1.0
+# Sources:
+#  [1] man sudo | Look at Environment $SUDO_USER - Opens the manual for sudo options and find what $SUDO_USER does
+#  [2] https://www.linode.com/docs/guides/linux-symlinks/ - How to Create Linux Symlinks
+#  [3] man ln | -L -s - Opens the manual for symlink options and find what -L and -s does
+# ==========================================================================================================================
+
+
 # Determine the original user's home directory
-if [ "$SUDO_USER" ];
+if [ "$SUDO_USER" ]; # $SUDO_USER: Set to the login name of the user who invoked sudo. [2]
 then
   # If the script is run with sudo, set USER_HOME to the home directory of the user who invoked sudo
   USER_HOME="/home/$SUDO_USER"
   echo "Script is being run with sudo by user $SUDO_USER"
-elif
+elif [ "$UID" -eq 0 ]; # checks if UID is 0. UID of 0 is the root user.
+then
   # If the script is run by the root user directly
-  [ "$EUID" -eq 0 ];
-  then
-    USER_HOME="/root"
-    echo "Script is being run by the root user"
+  USER_HOME="/root" # set USER_HOME to root user home
+  echo "Script is being run by the root user"
 else
   # If not run with sudo, set USER_HOME to the current user's home directory
-  USER_HOME=$HOME
+  USER_HOME=$HOME # set USER_HOME to current user's home
   echo "Script is being run by the current user $USER"
 fi
 
-
+#specifies where the 2420-as2-starting-files is located
 clone_dir=$USER_HOME/acit2420_shell_script/file_system_script/2420-as2-starting-files
 
 # Symbolic link for config files
 
 # symlink bin
-# Check if $USER_HOME/bin doesn't exist. If it doesn't created the bin dir, else, say it already exist.
-if [ ! -d $USER_HOME/bin ];
-then
+# Check if $USER_HOME/bin doesn't exist. Create it if necessary.
+if [ ! -d $USER_HOME/bin ]; then
   mkdir -p $USER_HOME/bin
   echo "/bin directory created"
 else
   echo "/bin directory already exists"
 fi
 
-# Check if the symlink exist with -L option. If it exist, say it exist, else symlink the two and say it has been created.
-if [ -L $USER_HOME/bin/sayhi ];
+# Check if the symlink exists with the option -L [3]. Create it if necessary [2].
+if [ -L $USER_HOME/bin/sayhi ]; # dereference TARGETs that are symbolic links [3]
 then
   echo "Symlink $USER_HOME/bin/sayhi already exists"
 else
-  ln -s $clone_dir/bin/sayhi $USER_HOME/bin/sayhi
+  ln -s $clone_dir/bin/sayhi $USER_HOME/bin/sayhi # The option -s make symbolic links instead of hard links [3]
   echo "Symlink $USER_HOME/bin/sayhi created"
 fi
 
+# Check if the symlink exists. Create it if necessary [2].
 if [ -L $USER_HOME/bin/install-fonts ];
 then
   echo "Symlink $USER_HOME/bin/install-fonts already exists"
@@ -51,7 +63,7 @@ else
 fi
 
 # symlink config for kak
-# Check if $USER_HOME/.config/kak doesn't exist. If it doesn't created the .config/kak dir, else, say it already exist.
+# Check if $USER_HOME/.config/kak doesn't exist [3]. Create it if necessary [2].
 if [ ! -d $USER_HOME/.config/kak ];
 then
   mkdir -p $USER_HOME/.config/kak
@@ -60,7 +72,7 @@ else
   echo "/.config/kak directory already exists"
 fi
 
-# If symlink exist, say it exist, else symlink the two and say it has been created.
+# Check if the symlink exists [3]. Create it if necessary [2].
 if [ -L $USER_HOME/.config/kak/kakrc ];
 then
   echo "Symlink $USER_HOME/.config/kak/kakrc already exists"
@@ -70,7 +82,7 @@ else
 fi
 
 # symlink config for tmux
-# Check if $USER_HOME/.config/tmux doesn't exist. If it doesn't created the .config/tmux dir, else, say it already exist.
+# Check if $USER_HOME/.config/tmux doesn't exist [2]. Create it if necessary [3].
 if [ ! -d $USER_HOME/.config/tmux ];
 then
   mkdir -p $USER_HOME/.config/tmux
@@ -79,7 +91,7 @@ else
   echo "/.config/tmux directory already exists"
 fi
 
-# If symlink exist, say it exist, else symlink the two and say it has been created.
+# Check if the symlink exists [2]. Create it if necessary [2].
 if [ -L $USER_HOME/.config/tmux/tmux.conf ];
 then
   echo "Symlink $USER_HOME/.config/tmux/tmux.conf already exists"
@@ -89,7 +101,7 @@ else
 fi
 
 # symlink home bashrc
-# If symlink exist, say it exist, else symlink the two and say it has been created.
+# Check if the symlink exists [3]. Create it if necessary [2].
 if [ -L $USER_HOME/.bashrc ];
 then
   echo "Symlink $USER_HOME/.bashrc already exists"
